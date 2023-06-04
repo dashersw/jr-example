745fa46da1614684b2b59a622570af52a7ff9609
Armagan Amcalar <armagan@amcalar.com>
2023-06-04 14:27:08 +0200

    Update index.js
    Co-authored-by: jr-2023[bot] <135511426+jr-2023[bot]@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // implement a fizz buzz function.
 
 function fizzBuzz(n) {
-  const result = [];
+  const result = []
   for (let i = 1; i <= n; i++) {
     if (i % 15 === 0) result.push('FizzBuzz')
     else if (i % 3 === 0) result.push('Fizz')
