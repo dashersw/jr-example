7a1ce94c79e617effaf196eb2d8c23ec096e94bc
Armagan Amcalar <armagan@amcalar.com>
2023-06-04 12:28:06 +0200

    Add initial example

---
+++ index.js
@@ -0,0 +1,14 @@
+// implement a fizz buzz function.
+
+function fizzBuzz(n) {
+  var result = []
+  for (var i = 1; i <= n; i++) {
+    if (i % 15 === 0) result.push('FizzBuzz')
+    else if (i % 3 === 0) result.push('Fizz')
+    else if (i % 5 === 0) result.push('Buzz')
+    else result.push(i.toString())
+  }
+  return result
+}
+
+console.log(fizzBuzz(15))
